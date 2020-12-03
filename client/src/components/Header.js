ecb623aa4f48efe3950619990fd2d39257b11c15
Anthony Siletti <anthony.siletti@gmail.com>
2020-12-03 12:25:01 -0500

    starting frontend

---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+
+const Header = () => {
+    return (
+        <Navbar bg='light' expand='md'>
+            
+        </Navbar>
+    )
+}
+
+export default Header
