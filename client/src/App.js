3aac54a2cf63b62f6bc922736be113da1e7ed6b0
Anthony Siletti <anthony.siletti@gmail.com>
2020-12-03 13:05:52 -0500

    progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import Header from './components/Header';
+import Container from 'react-bootstrap/Container';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
 
 const App = () => {
-  return <></>;
+  return (
+    <Router>
+      <Header />
+      <main className='py-2'>
+        <Container>
+          <Route exact path='/' component={LandingPage} />
+        </Container>
+      </main>
+    </Router>
+  );
 };
 
 export default App;
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,28 @@
-import React from 'react'
-import Navbar from 'react-bootstrap/Navbar'
+import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 const Header = () => {
-    return (
-        <Navbar bg='light' expand='md'>
-            
-        </Navbar>
-    )
-}
+  //@todo User Link below
+  return (
+    <Navbar bg='light' expand='md'>
+      <LinkContainer to='/'>
+        <Navbar.Brand>BRAND</Navbar.Brand>
+      </LinkContainer>
+      <Navbar.Toggle aria-controls='main-navbar' />
+      <Navbar.Collapse id='main-navbar'>
+        <Nav className=''>
+          <LinkContainer to='/'>
+            <Nav.Link>Home</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to='/recipes'>
+            <Nav.Link>Recipes</Nav.Link>
+          </LinkContainer>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
-export default Header
+export default Header;
