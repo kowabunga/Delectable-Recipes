d5b30f0a863ee72d0253e15cd7aba8fb3db6836d
Anthony Siletti <anthony.siletti@gmail.com>
2020-12-13 16:05:30 -0500

    homepage good

--- client/src/components/RecipeCard.js
+++ client/src/components/RecipeCard.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import CardImg from 'react-bootstrap/esm/CardImg';
 
 const RecipeCard = ({ recipe }) => {
-  const { user, recipeTitle, recipeDescription, recipeImage } = recipe;
+  const { user, recipeTitle, recipeDescription, recipeImage, _id } = recipe;
   return (
-    <Card style={{ width: '18rem', height: '25rem' }} className='m-2'>
+    <Card className='m-2 recipe-card'>
       <CardImg variant='top' src={recipeImage} />
       <Card.Body>
         <Card.Title>{recipeTitle}</Card.Title>
         <Card.Subtitle className='mb-2 text-muted'>By {user}</Card.Subtitle>
-        <Card.Text>{recipeDescription}</Card.Text>
+        <Card.Text className='two-lines-only'>{recipeDescription}</Card.Text>
+        <Link
+          to={`/recipes/${_id}`}
+          className='link-underline recipe-card-link-position'
+        >
+          View Recipe <i className='fas fa-arrow-circle-right'></i>
+        </Link>
       </Card.Body>
     </Card>
   );
